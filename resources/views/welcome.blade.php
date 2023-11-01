466925bf3ea9580eef117c658305027327f69a98
Elias Jaimes <joabsito1724@gmail.com>
2023-11-01 07:19:59 -0700

    Creando una base de codigo del html en welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
- probando haber que pedo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Prueba para cualquier cosa</h1>
+</body>
+</html>
