ee95dcd2f847d81ef4ec93bbd58cc28c303bf3ad
Elias Jaimes <joabsito1724@gmail.com>
2023-11-08 07:54:39 -0700

    Le cambie los estilos a unos mas mejorcitos(igual de feos agregue los layouts y ya :V

--- resources/views/Layouts/Layouts.blade.php
+++ resources/views/Layouts/Layouts.blade.php
@@ -3,11 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>@yield('title', config('app.name', 'Laravel'))</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <style>
+        body {
+            color: white; /* Cambia el color del texto a blanco */
+        }
+    </style>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    @yield('content')
-    <footer>
-    </footer>
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-800">
+        <!-- Page Content -->
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            <!-- Puedes agregar contenido al pie de página si es necesario -->
+        </footer>
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,36 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
 
-Route::get('/Historial', [NoteController::class, 'Historial'])->name('historial');
-Route::get('/Procedimientos_Create', [NoteController::class,'Procedimientos_create'])->name('procedimientos.create');
-Route::get('/Welcome', [NoteController::class,'Welcome'])->name('Welcome');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/historial', [NoteController::class, 'Historial'])->name('historial');
+Route::get('/procedimientos_create', [NoteController::class, 'Procedimientos_create'])->name('procedimientos.create');
+Route::get('/Welcome', [NoteController::class, 'Welcome'])->name('Welcome');
+
+require __DIR__.'/auth.php';
